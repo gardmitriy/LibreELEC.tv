414030e9eae3f03275bbf3b390a1b0cb7fd43dd7
alex <surkov.alex@gmail.com>
2017-04-10 00:18:33 +0300

    projects/Amlogic: add M10 S812

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -336,6 +336,10 @@ post_makeinstall_target() {
   xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "repository.libreelec.tv" $ADDON_MANIFEST
   xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "service.libreelec.settings" $ADDON_MANIFEST
 
+  if [ "$DEVICE" = "M10" ]; then
+    xmlstarlet ed -L --subnode "/addons" -t elem -n "addon" -v "script.service.m10vfd" $ADDON_MANIFEST
+  fi
+
   # more binaddons cross compile badness meh
   sed -e "s:INCLUDE_DIR /usr/include/kodi:INCLUDE_DIR $SYSROOT_PREFIX/usr/include/kodi:g" \
       -e "s:CMAKE_MODULE_PATH /usr/lib/kodi /usr/share/kodi/cmake:CMAKE_MODULE_PATH $SYSROOT_PREFIX/usr/share/kodi/cmake:g" \
